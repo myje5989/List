e7ee112e1790d4032545528d59b9b9e929a8dba0
myje5989 <30823860+myje5989@users.noreply.github.com>
2017-08-22 14:56:29 +0700

    Initial commit

---
+++ src/ListTest.java
@@ -0,0 +1,23 @@
+import java.text.CollationKey;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		ArrayList<String> list1 = new ArrayList<>();
+		list1.add("Kullanat");
+		list1.add("Sittichok");
+		list1.add("Pattamaporn");
+		for(String list:list1){
+			System.out.println(list);
+		}
+		
+		Collections.sort(list1);
+		
+		for(String list:list1){
+			System.out.println(list);
+		}
+	}
+
+}
